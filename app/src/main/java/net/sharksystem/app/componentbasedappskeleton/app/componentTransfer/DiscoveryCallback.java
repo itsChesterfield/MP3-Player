8d9be0d9052a30bf98d90964495cddda21c7d6d2
itsChesterfield <maxdanigel1204@gmail.com>
2026-07-08 11:45:05 +0200

    DiscoveryCallback Kommentar hinzugefügt.

--- app/src/main/java/net/sharksystem/app/componentbasedappskeleton/app/componentTransfer/DiscoveryCallback.java
+++ app/src/main/java/net/sharksystem/app/componentbasedappskeleton/app/componentTransfer/DiscoveryCallback.java
@@ -1,4 +1,7 @@
 package net.sharksystem.app.componentbasedappskeleton.app.componentTransfer;
 
+/**
+ * Die DiscoveryCallback Klasse ist dafür Zuständig, das der Controller nicht auf das Bluetooth Ergebnis warten muss.
+ */
 public interface DiscoveryCallback {
 }
