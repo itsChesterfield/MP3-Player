782374e3e54a781ea9f9b809d253ab1306ae44f7
itsChesterfield <maxdanigel1204@gmail.com>
2026-07-08 23:25:53 +0200

    anfang vom discoverDevices

--- app/src/main/java/net/sharksystem/app/componentbasedappskeleton/app/componentTransfer/TransferImpl.java
+++ app/src/main/java/net/sharksystem/app/componentbasedappskeleton/app/componentTransfer/TransferImpl.java
@@ -2,12 +2,24 @@ package net.sharksystem.app.componentbasedappskeleton.app.componentTransfer;
 
 import net.sharksystem.app.componentbasedappskeleton.app.model.Device;
 import net.sharksystem.app.componentbasedappskeleton.app.model.MP3File;
+import android.bluetooth.BluetoothAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransferImpl implements Transfer{
+    BluetoothAdapter bluetooth;
+    public TransferImpl(BluetoothAdapter bluetooth){
+        this.bluetooth = bluetooth;
+    }
+
+    /**
+     * Sucht die Bluetooth geräte in der Gegend.
+     * @param callback
+     */
     @Override
     public void discoverDevices(DiscoveryCallback callback) {
+        List<Device> devices = new ArrayList<>();
 
     }
 
